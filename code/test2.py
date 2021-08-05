e6ee0cbdb99c5f2fea8b2ca0e7e425f498cba765
finxg28 <finlaygerchen@gmail.com>
2021-08-05 15:57:28 +0100

    Added other code

---
+++ code/test2.py
@@ -0,0 +1,16 @@
+import json
+from pathlib import Path
+file = Path(__file__).parents[1]/'first-json-from-data-set.json'
+import pandas
+with open(file.name) as open_file:
+    df=json.load(open_file)
+    print(df)
+    df2= pandas.DataFrame.from_dict(df)
+    #print(df["messages"])
+    #print(df["players"])
+    #print(df.keys())
+    #for item in df.keys(): #
+        #print(df[item])
+        #frompathlib iterdir
+    print (df2)
+        
