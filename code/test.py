8aac19895517ccf9e9e5819c55d7d8b450239d7e
finxg28 <finlaygerchen@gmail.com>
2021-08-05 15:25:41 +0100

    code to parse json into python and formatt into pandas

--- code/test.py
+++ code/test.py
@@ -1,3 +1,5 @@
 import pandas as pd
-patients_df = pd.read_json('C:\\Users\\finch\\Documents\\roke work exprience\\diptest1.json')
-patients_df.head()
+path = "C:\\Users\\finch\\Documents\\roke work exprience\\"
+file = 'first-json-from-data-set.json'
+df = pd.read_json(path + file)
+print(df)
